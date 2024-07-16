8785aa82f728ecfd791521047dc8259dd32054c9
DGCodeWeb <dasg2506@icloud.com>
2024-07-16 02:20:16 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'poppins': ['"Poppins"', 'sans-serif'],
+				'opensans': ['"Open Sans Variable"', 'sans-serif']
+			}
+		},
+		colors: {
+			'pviolet': 'hsl(257, 40%, 49%)',
+			'psmagenta': 'hsl(300, 69%, 71%)'
+		}
+	},
+	plugins: [],
+}
